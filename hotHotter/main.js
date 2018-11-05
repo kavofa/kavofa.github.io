fd6c9ed6c7ce6155e044c0d6558ce33a822b07e4
kavofa <Karl@kavofa.domain.name>
2018-11-05 21:59:58 +0100

    new mode

---
+++ hotHotter/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+var c = -1;
+    function reload() {
+    c = c+1;
+    $('#counter').text(c)
+    var p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
+    var p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
+    $('#p1').text(p1);
+    $('#img1').attr("src", log[p1]);
+    $('#img1').attr("alt", p1);
+    $('#p2').text(p2);
+    $('#img2').attr("src", log[p2]);
+    $('#img2').attr("alt", p2);
+}
+    $("#c1").click(function() {
+        reload();
+    })
+    $("#c2").click(function() {
+        reload();
+    })
+reload()
+});
