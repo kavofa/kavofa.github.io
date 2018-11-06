6206d3973a7dc05cfdd72459d9cf430467bb1ae9
kavofa <Karl@kavofa.domain.name>
2018-11-06 19:06:36 +0100

    fixed ee at 100

--- hotHotter/main.js
+++ hotHotter/main.js
@@ -19,8 +19,8 @@ var p2 = {};
         $('#img1').attr("src", log[p1]);
         $('#img1').attr("alt", p1);
         $('#p2').text('Gigig Hadid');
-        $('#img2').attr("src", 'Gigi Hadid');
-        $('#img2').attr("alt", 'https://i.pinimg.com/originals/bb/91/07/bb9107ea936d0402ae8eb7d26e1d0bcd.jpg');
+        $('#img2').attr("src", 'https://i.pinimg.com/originals/bb/91/07/bb9107ea936d0402ae8eb7d26e1d0bcd.jpg');
+        $('#img2').attr("alt", 'Gigig Hadid');
     } else {
         p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
         p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
