d7269fe2e6e13512cb665348c285ad3512a65ad3
kavofa <Karl@kavofa.domain.name>
2018-11-06 19:03:37 +0100

    fixed easter egg

--- hotHotter/main.js
+++ hotHotter/main.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
 var c = -1;
-//Just for debug purposes
+var p1 = {};
+var p2 = {};
     function reload() {
     c = c+1;
     $('#counter').text(c)
     if (c == 50){
-        var p1 = {'Summer Ray': 'https://i.redd.it/il5rxl288e6y.jpg'};
-        var p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
-        $('#p1').text(p1);
-        $('#img1').attr("src", p1);
-        $('#img1').attr("alt", p1);
+        p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
+        $('#p1').text('Summer Ray');
+        $('#img1').attr("src", 'https://i.redd.it/il5rxl288e6y.jpg');
+        $('#img1').attr("alt", 'Summer Ray');
         $('#p2').text(p2);
         $('#img2').attr("src", log[p2]);
         $('#img2').attr("alt", p2);
     } else if (c == 100) {
-        var p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
-        var p2 = {'Gigi Hadid': 'https://i.pinimg.com/originals/bb/91/07/bb9107ea936d0402ae8eb7d26e1d0bcd.jpg'};
+        p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
         $('#p1').text(p1);
         $('#img1').attr("src", log[p1]);
         $('#img1').attr("alt", p1);
-        $('#p2').text(p2);
-        $('#img2').attr("src", p2);
-        $('#img2').attr("alt", p2);
+        $('#p2').text('Gigig Hadid');
+        $('#img2').attr("src", 'Gigi Hadid');
+        $('#img2').attr("alt", 'https://i.pinimg.com/originals/bb/91/07/bb9107ea936d0402ae8eb7d26e1d0bcd.jpg');
     } else {
-        var p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
-        var p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
+        p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
+        p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
         $('#p1').text(p1);
         $('#img1').attr("src", log[p1]);
         $('#img1').attr("alt", p1);
