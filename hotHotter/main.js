d5ea3bcdfe081531ff751c5f4b434bc9ccb562c9
kavofa <Karl@kavofa.domain.name>
2018-11-06 18:28:41 +0100

    added people

--- hotHotter/main.js
+++ hotHotter/main.js
@@ -3,8 +3,8 @@ var c = -1;
     function reload() {
     c = c+1;
     $('#counter').text(c)
-    var p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
-    var p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)];
+    var p1 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)] || Object.keys(models)[Math.floor(Math.random()*Object.keys(models).length)];
+    var p2 = Object.keys(log)[Math.floor(Math.random()*Object.keys(log).length)] || Object.keys(models)[Math.floor(Math.random()*Object.keys(models).length)];
     $('#p1').text(p1);
     $('#img1').attr("src", log[p1]);
     $('#img1').attr("alt", p1);
